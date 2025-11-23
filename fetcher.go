4d5748060521f0b136520282ff8d831e23e31be2
mattholliday123 <mattholliday430@gmail.com>
2025-11-23 00:43:27 -0600

    further update for news

--- fetcher.go
+++ fetcher.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"os"
+	"io"
 	"log"
+	"net/http"
+	"os"
 
 	"github.com/joho/godotenv"
 )
+
+
+type g_response struct {
+	webUrl string `json:"webUrl"`
+}
 
 func main(){
 
@@ -15,7 +23,7 @@ func main(){
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-	//g_key := os.Getenv("G_KEY")
+	g_key := os.Getenv("G_KEY")
 	//search_key
 
 	//exec, type(wiki, news, search), extra arguments
@@ -26,10 +34,42 @@ func main(){
 		arg_string = arg_string + extra_args[i]
 	}
 
+	var g_url string
+	var m g_response
+
 	//switch different fetch types
 	switch fetch_type {
 	case "news":
+		//default
+		//sections
+
+		//request
+		g_url = "https://content.guardianapis.com/sections?q=" + arg_string + "&api-key=" + g_key
+		response, err := http.Get(g_url)
+		if err != nil {
+			log.Fatal(err)
+		}
+		//json body
+		defer response.Body.Close()
+		if response.StatusCode != http.StatusOK {
+			fmt.Println("status code not ok")
+			os.Exit(1)
+		}
+		body, err := io.ReadAll(response.Body)
+		if err != nil{
+			log.Fatal(err)
+		}
+		//decode body
+		json_err := json.Unmarshal(body, &m)
+		if json_err != nil{
+			log.Fatal(json_err)
+		}
+		//return back to python script
+		fmt.Print(m.webUrl)
+
 	case "search":
+		//http.Get(url)
+		//err := json.Unmarshal(response, &m)
 	default:
 		fmt.Print("invalid option")
 	}
--- info.py
+++ info.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 
+from re import L
 import wikipediaapi
 import requests
 import sys 
+import subprocess
+import json
+from bs4 import BeautifulSoup
 from rich import print
 from rich.panel import Panel
 from rich.console import Console
@@ -24,78 +28,99 @@ def print_sections(sections):
         print(f"{index}: [bold]{s.title}[/bold] - {s.text[0:40]}...")
         index += 1
 
-page_input = " ".join(sys.argv[1:])
+#options
+#TODO: -dev flag for embed nvim
+match sys.argv[1]:
+    case 'news':
+        result = subprocess.run(["./fetcher.go", sys.argv[2]], capture_output=True, text=True)
+        news_url = json.loads(result.stdout)
+        #Beatiful Soup to render html
+        html = requests.get(news_url).text
+        soup = BeautifulSoup(html, "html.parser")
+    case 'wiki':
+        #arguments for wiki summary
+        page_input = " ".join(sys.argv[1:])
 
-if page_input == 'help' or page_input == 'h': 
-    display_help()
+        if page_input == 'help' or page_input == 'h': 
+            display_help()
 
-sections_dict = {}
+        sections_dict = {}
 
-url = "https://en.wikipedia.org/w/api.php"
-params = {
-        "action": "opensearch",
-        "namespace": "0",
-        "search": page_input,
-        "limit": "5",
-        "format": "json"
-        }
-headers = {
-        "User-Agent": "infocli/1.0 (myemail@example.com)"
-        }
+        #request setup 
+        url = "https://en.wikipedia.org/w/api.php"
+        params = {
+                "action": "opensearch",
+                "namespace": "0",
+                "search": page_input,
+                "limit": "5",
+                "format": "json"
+                }
+        headers = {
+                "User-Agent": "infocli/1.0 (myemail@example.com)"
+                }
 
-#these are results from input
-r = requests.get(url, params=params, headers=headers)
-data = [];
-if r.status_code == 200:
-    data = r.json()
-else:
-    print(r.status_code)
-
-titles = data[1]
-title_page = titles[0]
-
-#fetch the page
-wiki_wiki = wikipediaapi.Wikipedia(user_agent='infocli/1.0 (myemail@example.com)', language='en')
-page_py = wiki_wiki.page(title_page)
-
-if not page_py.exists():
-    print("Page does not exist")
-    exit(1)
-
-#print info
-print("[bold]Title:[/bold] %s\n" % page_py.title)
-print(Panel(page_py.summary, title="Summary"))
-p_url = page_py.fullurl
-print(f"[blue]{p_url}\n")
-while(True):
-    print_sections(page_py.sections)
-    sec_input = input('Input\n')
-    #exit program
-    if sec_input == 'q': 
-        exit(1)
-    #highlight keyword TODO: fuzzy?
-    elif sec_input[0] == '/': 
-        sec_input = sec_input[1:]
-        console = Console()
-        console.clear()
-        text = Text(page_py.summary)
-        text.highlight_words([sec_input], style = "bold blue", case_sensitive =False)
-        print(Panel(text, title="Summary"))
-        continue
-    #get integer value of input
-    try:
-        sec_input = int(sec_input)
-    except ValueError:
-        print("  invalid input\n")
-        display_help()
-    selected_section = sections_dict.get(sec_input)
-    #sections logic
-    if not selected_section:
-        print("Not valid section")
-    else:
-        section = page_py.section_by_title(selected_section)
-        if section is not None:
-            print(Panel(section.text, title=section.title))
+        #these are results from input
+        r = requests.get(url, params=params, headers=headers)
+        data = [];
+        if r.status_code == 200:
+            data = r.json()
         else:
-            print("section is invalid")
+            print(r.status_code)
 
+        if not data[1]:
+            print("No valid pages found")
+            exit(1)
+
+        #title pages, as well as print available pages for help
+        titles = data[1]
+        print("Available pages")
+        print(data[1])
+        title_page = titles[0]
+
+        #fetch the page
+        wiki_wiki = wikipediaapi.Wikipedia(user_agent='infocli/1.0 (myemail@example.com)', language='en')
+        page_py = wiki_wiki.page(title_page)
+
+        if not page_py.exists():
+            print("Page does not exist")
+            exit(1)
+
+        #print info
+        print("[bold]Title:[/bold] %s\n" % page_py.title)
+        print(Panel(page_py.summary, title="Summary"))
+        p_url = page_py.fullurl
+        print(f"[blue]{p_url}\n")
+        while(True):
+            print_sections(page_py.sections)
+            sec_input = input('Input\n')
+            #exit program
+            if sec_input == 'q': 
+                exit(1)
+            #highlight keyword TODO: fuzzy?
+            elif sec_input[0] == '/': 
+                sec_input = sec_input[1:]
+                console = Console()
+                console.clear()
+                text = Text(page_py.summary)
+                text.highlight_words([sec_input], style = "bold blue", case_sensitive =False)
+                print(Panel(text, title="Summary"))
+                continue
+            #get integer value of input
+            try:
+                sec_input = int(sec_input)
+            except ValueError:
+                print("  invalid input\n")
+                display_help()
+            selected_section = sections_dict.get(sec_input)
+            #sections logic
+            if not selected_section:
+                print("Not valid section")
+            else:
+                section = page_py.section_by_title(selected_section)
+                if section is not None:
+                    print(Panel(section.text, title=section.title))
+                else:
+                    print("section is invalid")
+
+    case _:
+        display_help()
